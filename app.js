7536317160f3836e35479c972374d87ec3582d78
‪Alaaddin yahia <150624261+ALAAEDDIN97120@users.noreply.github.com>
2025-05-14 23:59:08 +0300

    feat: Preparing the master file

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config;
+const express = require("express");
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get("", (req, res) => {
+	res.send("Hello");
+});
+
+app.listen(PORT, () => {
+	console.log("welcome in Port 5000");
+});
